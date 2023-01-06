23e8f6ddaea96f4020e7759ce0ce2d8a6daf2dde
ENRICO MAGLIANO s295692 <s295692@studenti.polito.it>
2023-01-06 17:24:34 +0100

    add figthing function

--- quarto/main.py
+++ quarto/main.py
@@ -19,12 +19,42 @@ class RandomPlayer(quarto.Player):
     def place_piece(self) -> tuple[int, int]:
         return random.randint(0, 3), random.randint(0, 3)
 
+def play_one_game(game: quarto.Quarto, player1: quarto.Player, player2: quarto.Player):
+    '''
+    Play one game player1 against player2, print the winner
+    '''
+    game.set_players((player1, player2))
+    winner = game.run()
+    logging.warning(f"main: Winner: player {winner}")
+
+def play_n_game(game: quarto.Quarto, player1: quarto.Player, player2: quarto.Player, n: int):
+    '''
+    Play n games player1 against player2, print the winner ratio of player1 over player2, switching the starter at each game
+    '''
+
+    win_count = 0
+    last_start = 1
+    for i in range(n):
+        game.reset()
+
+        if last_start == 1:
+            game.set_players((player1, player2))
+            last_start = 0
+        else:
+            game.set_players((player2, player1))
+            last_start = 1
+
+        winner = game.run()
+        
+        if (winner == 0 and last_start == 0) or (winner == 1 and last_start == 1): #player1 win
+            win_count+=1
+                    
+    logging.warning(f"main: Winner ratio of player1: {win_count/n}")        
 
 def main():
     game = quarto.Quarto()
-    game.set_players((RandomPlayer(game), RandomPlayer(game)))
-    winner = game.run()
-    logging.warning(f"main: Winner: player {winner}")
+    #play_one_game(game, RandomPlayer(game), RandomPlayer(game))
+    play_n_game(game, RandomPlayer(game), RandomPlayer(game), 100)
 
 
 if __name__ == '__main__':
