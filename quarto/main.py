10603fc92f58ff2a3335ce720f60a105f07e132e
EnricoMagliano <enrico.magliano@outlook.com>
2023-01-17 00:03:49 +0100

    update  result

--- quarto/main.py
+++ quarto/main.py
@@ -57,7 +57,7 @@ def play_n_game(game: quarto.Quarto, player1: quarto.Player, player2: quarto.Pla
 def main():
     game = quarto.Quarto()
     #play_one_game(game, RandomPlayer(game), RandomPlayer(game))
-    play_n_game(game, myPastimes.MyPastimes(game), RandomPlayer(game), 10000)
+    play_n_game(game, myPastimes.MyPastimes(game), myMinMax.MyMinMax(game), 1000)
 
 
 if __name__ == '__main__':
