fd6b4bae6ffd82a8145ed4412cadf18b1ea521cc
EnricoMagliano <enrico.magliano@outlook.com>
2023-01-19 18:57:46 +0100

    rf v000

--- quarto/agentRL.py
+++ quarto/agentRL.py
@@ -18,20 +18,17 @@ class ReinforcementLearning(quarto.Player):
         self.current = dict()
 
     def set_learning(self, value: bool):
+        '''set RF agent in learning mode if value equal true, otherwise set it on evaluation mode.'''
         self.learning = value
 
     def save_knowledge(self, win):
-        #print(self.current)
-        #print(self.knowledge)
+        '''save the move made in this game scoring accordinng with the outcome (win)'''
         for board, value in self.current.items():
-            #print("board ", board)
-            #print("value ", value)
             if board in self.knowledge:
                 if "choose_piece" in value:
                     not_in = True 
                     for element in self.knowledge[board]["choose_piece"]:
                         if element[0] == value["choose_piece"]:
-                            #print("err ", element)
                             not_in = False
                             element[1]+= 1 if win else -1
                     if not_in:
@@ -54,34 +51,39 @@ class ReinforcementLearning(quarto.Player):
         self.current = dict()
 
     def choose_piece(self) -> int:
+        board = self.get_game().get_board_status()
+        free_pieces = list(utilities.free_pieces(self).keys())           
+        choose = random.choice(free_pieces)
         if self.learning:
-            board = self.get_game().get_board_status()
-            free_pieces = list(utilities.free_pieces(self).keys())           
-            choose = random.choice(free_pieces)
-            print("in choose piece: ", choose)
-            print(self.current)
-            print("board ", np.array2string(board))
-            
             self.current[np.array2string(board)] = {"choose_piece": choose}
-            print(self.current)
             return choose
         else:
-            return random.randint(0, 15)    
+            best = None
+            for piece_score in self.knowledge[np.array2string(board)]["choose_piece"]:
+                if best == None or best[1] < piece_score[1]:
+                    best = piece_score
+            if best != None:
+                return best[0]
+            else:
+                return choose
 
     def place_piece(self) -> tuple[int, int]:
+        board = self.get_game().get_board_status()
+        free_place = utilities.free_place(self)
+        choose = random.choice(free_place)
         if self.learning:
-            board = self.get_game().get_board_status()
-            free_place = utilities.free_place(self)
-            choose = random.choice(free_place)
-            print("in place piece: ", choose)
-            print(self.current)
-            print("board ", np.array2string(board))
             
             self.current[np.array2string(board)] = {"place_piece": choose}
-            print(self.current)
             return choose[1], choose[0]
         else:
-            return random.randint(0, 3), random.randint(0, 3)
+            best = None
+            for place_score in self.knowledge[np.array2string(board)]["place_piece"]:
+                if best == None or best[1] < place_score[1]:
+                    best = place_score
+            if best != None:
+                return best[0][1], best[0][1]
+            else:
+                return choose[1], choose[0]
 
 
 
@@ -91,7 +93,7 @@ class ReinforcementLearning(quarto.Player):
 
 def play_n_game_train(game: quarto.Quarto, RF: ReinforcementLearning, player2: quarto.Player, n: int):
     '''
-    Play n games player1 (Reinforcement Learning) against player2, print the winner ratio of player1 over player2, switching the starter at each game
+    Play n games  for training player1 (Reinforcement Learning) against player2, print the winner ratio of player1 over player2, switching the starter at each game
     '''
 
     win_count = 0
@@ -114,14 +116,41 @@ def play_n_game_train(game: quarto.Quarto, RF: ReinforcementLearning, player2: q
         else: 
             RF.save_knowledge(False)
                     
-    logging.warning(f"main: Winner ratio of player1: {win_count/n}")  
+    logging.warning(f"main: Winner ratio of RF during training: {win_count/n}")  
+
+def play_n_game(game: quarto.Quarto, RF: ReinforcementLearning, player2: quarto.Player, n: int):
+    '''
+    Play n games player1 (Reinforcement Learning) against player2, print the winner ratio of player1 over player2, switching the starter at each game
+    '''
+
+    win_count = 0
+    last_start = 1
+    for i in range(n):
+        game.reset()
+
+        if last_start == 1:
+            game.set_players((RF, player2))
+            last_start = 0
+        else:
+            game.set_players((player2, RF))
+            last_start = 1
+
+        winner = game.run()
+        
+        if (winner == 0 and last_start == 0) or (winner == 1 and last_start == 1): #player1 win
+            win_count+=1
+                    
+    logging.warning(f"main: Winner ratio of RF evaluation training: {win_count/n}")
 
 def training():
     game = quarto.Quarto()
     #play_one_game(game, RandomPlayer(game), RandomPlayer(game))
     agentReinLear = ReinforcementLearning(game)
     agentReinLear.set_learning(True)
-    play_n_game_train(game, agentReinLear, main.RandomPlayer(game), 100)     
+    play_n_game_train(game, agentReinLear, main.RandomPlayer(game), 1000) 
+    agentReinLear.set_learning(False) 
+    play_n_game(game, agentReinLear, main.RandomPlayer(game), 100) 
+
 
 if __name__ == '__main__':
     training()
